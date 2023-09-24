433a9e9e2ddd6795b3d5c5af531fd7ce7ad7e681
Lucas <lucas.villaume8@etu.univ-lorraine.fr>
2023-09-24 17:56:54 +0200

    Ajout d'un attribut pour Case

--- src/jeu/Case.java
+++ src/jeu/Case.java
@@ -1,4 +1,15 @@
 package jeu;
 
 public abstract class Case {
+    protected boolean blocking = false; //La case est-elle bloquante ?
+    //TODO: Attribut pour la texture ?
+    public Case(){}
+
+    public boolean isBlocking() {
+        return blocking;
+    }
+
+    public void setBlocking(boolean isBlocking) {
+        this.blocking = isBlocking;
+    }
 }
