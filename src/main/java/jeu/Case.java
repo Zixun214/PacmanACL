1f8846fe482ebdeab25c41fdcf8b066d6e793cbf
Lucas <lucas.villaume8@etu.univ-lorraine.fr>
2023-12-13 19:05:14 +0100

    Mise à jour uml

--- src/main/java/jeu/Case.java
+++ src/main/java/jeu/Case.java
@@ -5,7 +5,6 @@ import java.awt.*;
 public abstract class Case {
 
     protected boolean blocking = false; //La case est-elle bloquante ?
-    //TODO: Attribut pour la texture ?
     public Case(){}
 
     public void event(Joueur player){
